64224a2bff2563299a9994c5df5d609a1841db20
Bajracharya <bajracj@38f9d3dac8ef.ant.amazon.com>
2020-01-22 13:03:01 -0800

    databse connection established

--- src/main/java/com/jbajracharya/songr/Album.java
+++ src/main/java/com/jbajracharya/songr/Album.java
@@ -1,11 +1,24 @@
 package com.jbajracharya.songr;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import javax.persistence.*;
+
+//make a table for the class
+@Entity
 public class Album {
+    // id is primary key
+    // generated value makes it start at one and increment
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
     private String title;
     private String artist;
     private int songCount;
     private int seconds;
     private String imageUrl;
+
 
 
     public String getTitle() {
@@ -36,6 +49,9 @@ public class Album {
         this.imageUrl = imageUrl;
     }
 
+    public Album() {
+
+    }
 
     public void setTitle(String title) {
         this.title = title;
